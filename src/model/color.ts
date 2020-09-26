2d129e73c77fb9ebfe4bb53772b3bbeb1058b56f
kamilf2002 <63774240+kamilf2002@users.noreply.github.com>
2020-09-26 22:32:27 +0300

    Add files via upload

---
+++ src/model/color.ts
@@ -0,0 +1,21 @@
+const enum Color {
+	RED = "red",
+	GREEN = "green",
+	BLUE = "blue",
+	BLACK = "black",
+	PURPLE = "purple",
+	BROWN = "brown",
+	CYAN = "cyan",
+	WHITE = "white",
+	GRAY = "gray",
+	LIGHTBLUE = "lightblue",
+	LIGHTGREEN = "lightgreen",
+	PINK = "pink",
+	YELLOW = "yellow",
+	ORANGE = "orange",
+	BRIGHTWHITE = "brightwhite",
+	DARKBLUE = "darkblue",
+	DARKGREEN = "darkgreen",
+}
+
+export {Color}

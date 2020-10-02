d191ecb1de0cf12b41f06dc40869acdffde0bee5
Rjomba2000 <61803381+Rjomba2000@users.noreply.github.com>
2020-10-03 00:29:57 +0300

    Изменил класс Font на тип

--- src/model/Font.ts
+++ src/model/Font.ts
@@ -1,73 +1,9 @@
-import {BaseObject} from "../base/BaseObject";
-import {IEventDispatcher} from "../base/observer/IEventDispatcher";
-
-class Font extends BaseObject {
-    private _fontName: string;
-    private _fontSize: number;
-    private _isBold: boolean;
-    private _isItalic: boolean;
-    private _isUnderlined: boolean;
-    private readonly _fontChangeEvent: IEventDispatcher;
-
-    constructor() {
-        super();
-        this._fontName = "";
-        this._fontSize = 0;
-        this._isBold = false;
-        this._isItalic = false;
-        this._isUnderlined = false;
-        this._fontChangeEvent = this._createEventDispatcher();
-    }
-
-    fontSize(): number {
-        return this._fontSize;
-    }
-
-    fontName(): string {
-        return this._fontName;
-    }
-
-    isBold(): boolean {
-        return this._isBold;
-    }
-
-    isItalic(): boolean {
-        return this._isItalic;
-    }
-
-    isUnderlined(): boolean {
-        return this._isUnderlined;
-    }
-
-    fontChangeEvent(): IEventDispatcher {
-        return this._fontChangeEvent;
-    }
-
-    setFontSize(fontSize: number) {
-        this._fontSize = fontSize;
-        this._fontChangeEvent.dispatch();
-    }
-
-    setFontName(fontName: string) {
-        this._fontName = fontName;
-        this._fontChangeEvent.dispatch();
-    }
-
-    setBold(state: boolean) {
-        this._isBold = state;
-        this._fontChangeEvent.dispatch();
-    }
-
-    setUnderlined(state: boolean) {
-        this._isUnderlined = state;
-        this._fontChangeEvent.dispatch();
-    }
-
-    setItalic(state: boolean) {
-        this._isItalic = state;
-        this._fontChangeEvent.dispatch();
-    }
-
+type Font = {
+    fontName: string,
+    fontSize: number,
+    isBold: boolean,
+    isItalic: boolean,
+    _isUnderlined: boolean
 }
 
-export {Font}
+export type {Font}

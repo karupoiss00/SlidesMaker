918310fcaeafed696192aadd49a78b746984bb00
Rjomba2000 <61803381+Rjomba2000@users.noreply.github.com>
2020-10-03 00:31:35 +0300

    Изменил класс Style на тип

--- src/model/Style.ts
+++ src/model/Style.ts
@@ -1,50 +1,7 @@
-import {BaseObject} from "../base/BaseObject";
-import {IEventDispatcher} from "../base/observer/IEventDispatcher";
-
-class Style extends BaseObject {
-    private _backgroundColor: string;
-    private _strokeColor: string;
-    private _strokeWidth: number;
-    private readonly _styleChangeEvent: IEventDispatcher;
-
-    constructor() {
-        super();
-        this._backgroundColor = "#ffffff";
-        this._strokeColor = "#000000";
-        this._strokeWidth = 1;
-        this._styleChangeEvent = this._createEventDispatcher();
-    }
-
-    backgroundColor(): string {
-        return this._backgroundColor;
-    }
-
-    strokeColor(): string {
-        return this._strokeColor;
-    }
-
-    strokeWidth(): number {
-        return this._strokeWidth;
-    }
-
-    setBackgroundColor(color: string) {
-        this._backgroundColor = color;
-        this._styleChangeEvent.dispatch();
-    }
-
-    setStrokeColor(color: string) {
-        this._strokeColor = color;
-        this._styleChangeEvent.dispatch();
-    }
-
-    setStrokeWidth(width: number) {
-        this._strokeWidth = width;
-        this._styleChangeEvent.dispatch();
-    }
-
-    styleChangeEvent(): IEventDispatcher {
-        return this._styleChangeEvent;
-    }
+type Style = {
+    backgroundColor: string,
+    strokeColor: string,
+    strokeWidth: number
 }
 
-export {Style}
+export type {Style}

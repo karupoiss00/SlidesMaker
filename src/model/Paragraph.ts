54f8da6cb6c3df02407ec78a7e6674f5d160895b
Rjomba2000 <61803381+Rjomba2000@users.noreply.github.com>
2020-10-03 00:30:35 +0300

    Изменил класс Paragraph на тип

--- src/model/Paragraph.ts
+++ src/model/Paragraph.ts
@@ -1,30 +1,7 @@
-import {BaseObject} from "../base/BaseObject";
-import {IEventDispatcher} from "../base/observer/IEventDispatcher";
 import {Alignment} from "./Alignment";
 
-class Paragraph extends BaseObject {
-    private _alignmentState: Alignment
-    private readonly _paragraphChangeEvent: IEventDispatcher
-
-    constructor() {
-        super();
-        this._alignmentState = Alignment.LEFT;
-        this._paragraphChangeEvent = this._createEventDispatcher();
-    }
-
-    alignmentState(): Alignment {
-        return this._alignmentState;
-    }
-
-    setAlignmentState(state: Alignment) {
-        this._alignmentState = state;
-        this._paragraphChangeEvent.dispatch();
-    }
-
-    paragraphChangeEvent() {
-        return this._paragraphChangeEvent;
-    }
-
+type Paragraph = {
+    _alignmentState: Alignment
 }
 
-export {Paragraph}
+export type {Paragraph}

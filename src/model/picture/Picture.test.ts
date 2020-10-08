a7087e71005ccb16e95f1444b56e194f0306e925
Daniil <dr.chashka@gmail.com>
2020-10-09 00:09:15 +0300

    Были написаны тесты для Picture и TextBox.

---
+++ src/model/picture/Picture.test.ts
@@ -0,0 +1,9 @@
+import {createRect} from "../types/Rect";
+import { createPicture } from './Picture';
+
+test('Picture: создание картинки', () => {
+    expect({
+        src: 'C:/Pictures/picture.jpeg',
+        rect: createRect(),
+    }).toStrictEqual(createPicture('C:/Pictures/picture.jpeg'));
+});
---
+++ src/model/textbox/TextBox.test.ts
@@ -0,0 +1,13 @@
+import {createRect} from "../types/Rect";
+import {createParagraph} from "../types/Paragraph";
+import {createFont} from "../types/Font";
+import {createTextBox} from "./TextBox";
+
+test('TextBox: создание окна с текстом', () => {
+    expect({
+        text: '',
+        rect: createRect(),
+        paragraph: createParagraph(),
+        font: createFont(),
+    }).toStrictEqual(createTextBox());
+});

74527b1b4c0de0cfecf1e8cd863e082749b8684d
Rjomba2000 <61803381+Rjomba2000@users.noreply.github.com>
2020-10-10 00:13:53 +0300

    Добавлено тестирование enum Alignment

--- src/model/types/paragraph/Paragraph.tests.ts
+++ src/model/types/paragraph/Paragraph.tests.ts
@@ -1,6 +1,18 @@
 import {Alignment} from "../Alignment";
 import {createParagraph} from "./Paragraph";
 
+describe('импорт Alignment', () => {
+    it('должен понимать Alignment.LEFT', () => {
+        expect(Alignment.LEFT).toEqual('left');
+    });
+    it('должен понимать Alignment.CENTER', () => {
+        expect(Alignment.CENTER).toEqual('center');
+    });
+    it('должен понимать Alignment.RIGHT', () => {
+        expect(Alignment.RIGHT).toEqual('right');
+    });
+})
+
 describe("Paragraph.ts", () => {
     test('Paragraph.ts: createParagraph', () => {
         expect({

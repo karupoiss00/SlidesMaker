a93a1d2088359f864ea32c04ad2bccda3c102d57
karupoiss00 <glizeron1108@gmail.com>
2020-10-12 23:25:05 +0300

    Модель + ReactApp

---
+++ src/model/types/Rect.ts
@@ -0,0 +1,17 @@
+export type Rect = {
+    x: number;
+    y: number;
+    width: number;
+    height: number;
+};
+
+function createRect(): Rect {
+    return {
+        x: 0,
+        y: 0,
+        width: 0,
+        height: 0,
+    };
+}
+
+export { createRect };
---
+++ src/model/types/Style.ts
@@ -0,0 +1,17 @@
+import { Colors } from './Colors';
+
+export type Style = {
+    backgroundColor: Colors;
+    strokeColor: Colors;
+    strokeWidth: number;
+};
+
+function createStyle(): Style {
+    return {
+        backgroundColor: Colors.WHITE,
+        strokeColor: Colors.BLACK,
+        strokeWidth: 2,
+    };
+}
+
+export { createStyle };

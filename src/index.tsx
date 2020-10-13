704a517fefb9bbf2304e3ef79f4caa3236bb6e09
karupoiss00 <glizeron1108@gmail.com>
2020-10-13 22:09:59 +0300

    Реализован рендер текущего слайда и списка слайдов

--- src/index.tsx
+++ src/index.tsx
@@ -3,9 +3,23 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import {createSlidesMaker, SlidesMaker} from "./model/SlidesMaker";
+import {addSlide, createSlidesMaker, setBackground, setSelectedSlide, SlidesMaker} from "./model/SlidesMaker";
+import {setSlideBackgroundColor, setSlideBackgroundPicture} from "./model/slide/Slide";
+import {Colors} from "./model/types/Colors";
+import {createPicture, Picture} from "./model/slide/slide_objects/picture/Picture";
 
-const slidesMaker: SlidesMaker = createSlidesMaker();
+let slidesMaker: SlidesMaker = createSlidesMaker();
+
+const testBackgroundPicture: Picture = createPicture('https://sun9-59.userapi.com/c855736/v855736552/2ab80/5EcafNhcrDg.jpg');
+const testBackgroundPicture2: Picture = createPicture('https://sun9-19.userapi.com/c850220/v850220293/e9ca4/_eAdwybvugU.jpg');
+const testBackgroundColor: Colors = Colors.GREEN;
+
+slidesMaker = setBackground(slidesMaker, testBackgroundPicture);
+slidesMaker = addSlide(slidesMaker);
+slidesMaker = setBackground(slidesMaker, testBackgroundColor);
+slidesMaker = addSlide(slidesMaker);
+slidesMaker = setBackground(slidesMaker, testBackgroundPicture2);
+slidesMaker = setSelectedSlide(slidesMaker, 1);
 
 ReactDOM.render(
   <React.StrictMode>
--- src/view/slide/SlideView.tsx
+++ src/view/slide/SlideView.tsx
@@ -1,15 +1,37 @@
 import React from 'react'
 import {Slide} from "../../model/slide/Slide";
+import {Picture} from "../../model/slide/slide_objects/picture/Picture";
+import {Background} from "../../model/types/Background";
 
 interface SlideViewProps {
+    key: number;
     slide: Slide | null;
     className: string;
 }
 
 function SlideView(props: SlideViewProps) {
-    return (
-        <div className={props.className}></div>
-    )
+    let background: string;
+
+    if (props.slide !== null)
+    {
+        background = props.slide.background.toString();
+
+        if (typeof(props.slide.background) !== "string")
+        {
+            background = `no-repeat url(${props.slide.background.src})`;
+            console.log(background);
+        }
+
+        return (
+            <div className={props.className} style={{background: background, backgroundSize: "cover"}}></div>
+        )
+    }
+    else
+    {
+        return (
+            <div className={props.className}></div>
+        )
+    }
 }
 
 export {

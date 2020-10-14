667e665bd6ba548e502e2733213b205c9a0729ad
Daniil <dr.chashka@gmail.com>
2020-10-15 01:23:42 +0300

    Реализовано переключение слайдов

--- src/view/slidelist/SlideListView.tsx
+++ src/view/slidelist/SlideListView.tsx
@@ -2,12 +2,28 @@ import React from 'react'
 import {Slide} from "../../model/slide/Slide";
 import {SlideView} from "../slide/SlideView";
 import {SlidesMaker} from "../../model/SlidesMaker";
+import {SlidesMakerView} from "../SlidesMakerView";
+import ReactDOM from "react-dom";
+import App from "../../App";
 
 interface SlideViewProps {
     className: string;
     slidesMaker: SlidesMaker;
     slideList: Array<Slide>;
     currentSlide: number | null;
+}
+
+function changeSlide(slidesMaker: SlidesMaker, slideNumber: number | null) {
+    const newSlidesMaker = {...slidesMaker};
+    newSlidesMaker.currentSlide = slideNumber;
+
+    return ReactDOM.render(
+        <React.StrictMode>
+            <App appModel={newSlidesMaker} />
+        </React.StrictMode>,
+        document.getElementById('root')
+    );
+
 }
 
 function SlideListView(props: SlideViewProps) {
@@ -20,7 +36,7 @@ function SlideListView(props: SlideViewProps) {
          ? cssStyleName = "slide-view-icon-selected"
          : cssStyleName = "slide-view-icon";
 
-        return (<div className="slide-view-icon-container" key={slideNumber}>
+        return (<div className="slide-view-icon-container" key={slideNumber} onClick={() => {changeSlide(props.slidesMaker, slideNumber)}}>
             <SlideView
                 className={cssStyleName}
                 slidesMaker={props.slidesMaker}

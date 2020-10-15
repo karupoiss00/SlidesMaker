e1cd212bb56a2b6565b6fddaee6feffa980e66f3
karupoiss00 <glizeron1108@gmail.com>
2020-10-16 00:35:28 +0300

    Сделано добавление слайдов по кнопке и скролл

--- src/view/slidelist/SlideListView.tsx
+++ src/view/slidelist/SlideListView.tsx
@@ -1,10 +1,10 @@
-import React from 'react'
+import React, {MutableRefObject, useRef} from "react";
+import ReactDOM from "react-dom";
 import {Slide} from "../../model/slide/Slide";
 import {SlideView} from "../slide/SlideView";
-import {SlidesMaker} from "../../model/SlidesMaker";
-import {SlidesMakerView} from "../SlidesMakerView";
-import ReactDOM from "react-dom";
+import {SlidesMaker, addSlide} from "../../model/SlidesMaker";
 import App from "../../App";
+import {Button} from "../controls/Button";
 
 interface SlideViewProps {
     className: string;
@@ -23,8 +23,26 @@ function changeSlide(slidesMaker: SlidesMaker, slideNumber: number | null) {
         </React.StrictMode>,
         document.getElementById('root')
     );
-
 }
+
+const scrollSlideList = (className: string, deltaX: number) => {
+    const slideList = document.getElementsByClassName(className)[0];
+    slideList.scrollTo({
+        left: slideList.scrollLeft + deltaX,
+        behavior: "smooth",
+    });
+}
+
+function addNewSlide(slidesMaker: SlidesMaker) {
+    const newSlidesMaker = addSlide(slidesMaker);
+    return ReactDOM.render(
+        <React.StrictMode>
+            <App appModel={newSlidesMaker} />
+        </React.StrictMode>,
+        document.getElementById('root')
+    );
+}
+
 
 function SlideListView(props: SlideViewProps) {
     const listItems = props.slideList.map((value) => {
@@ -51,11 +69,23 @@ function SlideListView(props: SlideViewProps) {
         </div>)
 
     });
+
+
+
     return (
         <div className="slide-list-container">
+            <Button className="slide-list-scroll-button" text="<" onClick={ () => {
+                scrollSlideList(props.className, -600);
+            }}></Button>
             <div className={props.className}>
                 {listItems}
+                <Button className="slide-list-add-button" text="+" onClick={() => {
+                    addNewSlide(props.slidesMaker);
+                }}></Button>
             </div>
+            <Button className="slide-list-scroll-button" text=">" onClick={ () => {
+                scrollSlideList(props.className, 600);
+            }}></Button>
         </div>
     )
 }

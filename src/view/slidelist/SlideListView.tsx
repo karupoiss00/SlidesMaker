d1530505e42b3fe00df7f80e307e0983a7b89111
karupoiss00 <glizeron1108@gmail.com>
2020-10-17 23:39:04 +0300

    Обновлено позиционирование полоски текущего слайда

--- src/view/slidelist/SlideListView.tsx
+++ src/view/slidelist/SlideListView.tsx
@@ -1,7 +1,7 @@
 import React from "react";
+import {Button} from "../controls/Button";
 import {SlideView} from "../slide/SlideView";
 import {SlidesMaker, addSlide, setSelectedSlide} from "../../model/SlidesMaker";
-import {Button} from "../controls/Button";
 
 interface SlideListViewProps {
     className: string;

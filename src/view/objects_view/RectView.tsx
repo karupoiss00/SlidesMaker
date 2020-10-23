ceb17cad57060a5391e5ee3ecc2dc0efa2de7904
Daniil <dr.chashka@gmail.com>
2020-10-23 22:49:03 +0300

    Написан react-компонент для Rect

--- src/view/objects_view/RectView.tsx
+++ src/view/objects_view/RectView.tsx
@@ -4,7 +4,12 @@ import {Rect} from "../../model/types/Rect";
 
 export function RectView(props: Rect) {
     return (
-        <div className="rect" style={{width: props.width, height: props.height}}>
+        <div className="rect" style={{
+            left: props.x,
+            top: props.y,
+            width: props.width,
+            height: props.height,
+        }}>
             <div className={styles.rectDotLeftTop}></div>
             <div className={styles.rectDotLeftBottom}></div>
             <div className={styles.rectDotRightTop}></div>
